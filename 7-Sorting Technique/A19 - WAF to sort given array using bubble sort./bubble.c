342b42d006eb273d8ef04826421aac3ea4140b9c
Isaa Kazi <Isaakazi10@gmail.com>
2023-05-07 23:18:38 +0530

    Sorting Technique 1

--- 7-Sorting Technique/A19 - WAF to sort given array using bubble sort./bubble.c
+++ 7-Sorting Technique/A19 - WAF to sort given array using bubble sort./bubble.c
@@ -1,28 +1,17 @@
-/*
-Name        : ISAA KAZI
-Date        : 3/05/2023
-Description : A19 - WAF to sort given array using bubble sort.
-Sample I/O  :
-Sample O/P  :
-*/
 #include "main.h"
 
 // Defination of a function.
 void bubble(data_t *arr, int size)
 {
-    // for i is less than size minus 1/
     for (int i = 0; i < size - 1; i++)
     {
-        // for j is less than size
-        for (int j = i; j < (size); j++)
+        for (int j = 0; j < (size - i - 1); j++)
         {
-            // if arr[i] is less than arr[j]
-            if (arr[i] > arr[j])
+            if (arr[j] > arr[j + 1])
             {
-                // Interchange the number.
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }

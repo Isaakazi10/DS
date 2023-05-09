c8f3a1d71f66a67c39f4e38f208792e2b8e92b6a
Isaa Kazi <Isaakazi10@gmail.com>
2023-05-09 15:53:27 +0530

    Completed

--- 09-Hashing/0A27 - WAF to create hash table, to search data , to insert and delete element in hash table. Also to delete entire hash table./delete_HT.c
+++ 09-Hashing/0A27 - WAF to create hash table, to search data , to insert and delete element in hash table. Also to delete entire hash table./delete_HT.c
@@ -1,5 +1,31 @@
-#include"hash.h"
+#include "hash.h"
 
 int destroy_HT(hash_t *arr, int size)
 {
+    // i is less than size keep increamenting i.
+    for (int i = 0; i < size; i++)
+    {
+        // Storing arr address in temp pointer.
+        hash_t *temp = (arr + i);
+        temp->value = -1;
+
+        // While temp is not equal to NULL
+        while (temp != NULL)
+        {
+            // Copy temp in prev pointer and free prev pointer.
+            hash_t *prev = temp;
+
+            // Update the temp to next node.
+            temp = temp->link;
+
+            // In order to skip freeing the array.
+            if ((arr + i) != prev)
+            {
+                free(prev);
+            }
+        }
+
+        arr[i].link = NULL;
+    }
+    return SUCCESS;
 }
